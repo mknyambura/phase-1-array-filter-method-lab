02958b3c60695de69183d4c2f9bdcead2cd26cd8
mknyambura <kariuki.mercie@gmail.com>
2022-11-25 00:05:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const findMatching = (collection, names) => { 
+    return collection.filter(list => (
+        list.toLowerCase() === names.toLowerCase()
+    ));
+}
+
+const fuzzyMatch = (collection, names) => {
+    return collection.filter(list => (
+        list.slice(0, names.length) === names
+    ));
+}
+
+function matchName(collection, names){
+    return collection.filter(function(list){
+        return list.name === names;
+    })
+}
